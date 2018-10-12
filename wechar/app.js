908d755fae8f7ce61c9f788b355b07efec38fb7a
唐发登 <380919481@qq.com>
2018-10-12 17:21:39 +0800

    123

--- wechar/app.js
+++ wechar/app.js
@@ -3,13 +3,13 @@ var wxQuery = require('./utils/wxquery.js');
 //var apihelper = require('./utils/apihelper.js')
 App({ /** 全局引入wxQuery */
   wxQuery: wxQuery,
+  $: wxQuery.$,
   apihelper: require('./utils/apihelper.js').apihelper,
   hexMD5: require('./utils/apihelper.js').hexMD5,
   service: require('./utils/service').service,
   systemPms: require('./utils/service').systemPms,
   systemConfig: require('./utils/service').systemConfig,
   config: require('./utils/config').config,
-  $: wxQuery.$,
   onLaunch: function() {
     var self = this
     //本地存储
@@ -17,36 +17,88 @@ App({ /** 全局引入wxQuery */
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
+  },
+  alert: function (content, confirm_fun, title, cancel_fun) {    
+    wx.showModal({
+      title: title==undefined?"":title,
+      content: content,
+      showCancel: (confirm_fun != undefined || cancel_fun!=undefined),
+      confirmText: "确定",
+      success: function (res) {
+        //点击确定按钮
+        if (res.confirm && confirm_fun!=undefined) {
+          confirm_fun();
+        } else if (cancel_fun != undefined){
+          cancel_fun();
+        }
+      }
+    })
+  },
+  Ini: function() {
+    var self = this;
+    var userinfo = wx.getStorageSync('userinfo') || null;
+    if (userinfo == null) {
+      //用code 换 openid  调用接口
+      /*
+       // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
 
-          // 必须是在用户已经授权的情况下调用
-          wx.getUserInfo({
-            success: function(res) {
-              self.globalData.userInfo = res.userInfo
-              //var nickName = userInfo.nickName
-              //var avatarUrl = userInfo.avatarUrl
-              //var gender = userInfo.gender //性别 0：未知、1：男、2：女
-              //var province = userInfo.province
-              //var city = userInfo.city
-              //var country = userInfo.country
-            }
-          });
+
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            //start
+            wx.request({
+              url: '',
+              data: {
+                code: res.code,
+              },
+              success: function (res) {
+                console.log(res.data)
+                self.globalData.openid ="";// res.data;
+              }
+            })
+            //end
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
         }
-      }
-    })
+      })*/
+
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      // 获取用户信息
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            // 必须是在用户已经授权的情况下调用
+            wx.getUserInfo({
+              success: function(res) {
+                var userinfo = {
+                  nickName: res.userInfo.nickName,
+                  avatarUrl: res.userInfo.avatarUrl,
+                  gender: res.userInfo.gender, //性别 0：未知、1：男、2：女
+                  province: res.userInfo.province,
+                  city: res.userInfo.city,
+                  country: res.userInfo.country
+                }
+                wx.setStorageSync('userinfo', userinfo);
+                self.globalData.userInfo = res.userInfo;
+
+              }
+            });
+          }
+        }
+      })
+    }
+    return self.globalData.userInfo;
   },
   globalData: {
-    userInfo: null
+    userInfo: wx.getStorageSync('userinfo') || null,
+    openid: '321231'
   }
 })
--- wechar/pages/index/index.js
+++ wechar/pages/index/index.js
@@ -4,7 +4,7 @@ var app = getApp();
 
 Page({
   data: {
-    banners: {}, 
+    banners: {},
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -17,21 +17,26 @@ Page({
 
   /**
    * 用户点击右上角分享 转发 事件
-  */
-  onShareAppMessage: function () {
+   */
+  onShareAppMessage: function() {
 
-  }
-  ,
+  },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    app.wxQuery.register(this);
-    var self = this;
+  onLoad: function() {
+    app.wxQuery.register(this); 
 
+    app.Ini();
+    var self = this;
+    var userinfo = wx.getStorageSync('userinfo') || null;
+    self.setData({
+      userInfo: userinfo
+    });
+ 
     /*
       加载首页精彩活动/banner 配置参数
     */
@@ -45,89 +50,115 @@ Page({
     请求接口
     */
     wx.request({
-      url: app.apihelper.SetPms(pms), 
+      url: app.apihelper.SetPms(pms),
       method: 'POST',
-      header: {'content-type': 'application/json'},
-      success: function (result) {
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(result) {
         if (result != undefined && result != null && result != '' && result.data.head.response_code == "SUCCESS") {
-          self.setData({ banners: result.data.body.activitywonderfulactinfotextend });
+          self.setData({
+            banners: result.data.body.activitywonderfulactinfotextend
+          });
         }
       },
-      fail: function (ex) {
-      }
+      fail: function(ex) {}
     })
-    
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+  },
+  /**
+  * 生命周期函数--监听页面显示
+  */
+  onShow: function () {
+    var _this = this;
+    app.wxQuery.register(this);
+    app.$('paymentbtn').bind('tap', function (e) {
+      app.alert(
+        "确定去支付？",
+        function () {
+          wx.navigateTo({
+            url: "/pages/payment/pay/pay"
+          });
+        },
+        "支付提示",
+        function () {
+          app.alert("拉基吧倒")
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-  banner_gotolink: function (e) {
-    var self = this;
+      )
 
+    });
+
+  },
+
+  userinfobtnclick: function() {
+    //已封装 消息弹窗 (模态窗) app.alert("消息内容" , 点击确定执行方法 , “消息标题” , 点击取消执行方法)
+    app.alert(
+      "content123",
+      function() {
+        console.info(app.globalData.userInfo);
+      },
+      "传入提示",
+      function() {
+        app.alert("消息内容")
+      }
+    )
+  },
+  banner_gotolink: function(e) {
+    var self = this;
     var link = escape(e.currentTarget.dataset.link);
     var title = e.currentTarget.dataset.title;
-    wx.navigateTo({ url: "/pages/news/about/about?type=post&link=" + link + "&title=" + title})
+    wx.navigateTo({
+      url: "/pages/news/about/about?type=post&link=" + link + "&title=" + title
+    })
   },
-  changeIndicatorDots: function (e) {
+  changeIndicatorDots: function(e) {
     this.setData({
       indicatorDots: !this.data.indicatorDots
     })
   },
-  translate: function () {
-    setInterval(function () {
+  translate: function() {
+    setInterval(function() {
       this.animation.translate(0, 50).step()
-      this.setData({ animation: this.animation.export() })
+      this.setData({
+        animation: this.animation.export()
+      })
     }, 2000);
     this.animation.translate(0, 50).step()
-    this.setData({ animation: this.animation.export() })
+    this.setData({
+      animation: this.animation.export()
+    })
   },
-  changeAutoplay: function (e) {
+  changeAutoplay: function(e) {
     this.setData({
       autoplay: !this.data.autoplay
     })
   },
-  intervalChange: function (e) {
+  intervalChange: function(e) {
     this.setData({
       interval: e.detail.value
     })
   },
-  durationChange: function (e) {
+  durationChange: function(e) {
     this.setData({
       duration: e.detail.value
     })
   },
-  paymentbtnclick:function(e){
-    wx.navigateTo({ url: "/pages/payment/pay/pay"});
-  }
 })
+var indexevent = {
+  query :wx.createSelectorQuery(),
+  paymentbtnclick: function(e) {
+    app.alert(
+      "确定去支付？",
+      function() {
+        wx.navigateTo({
+          url: "/pages/payment/pay/pay"
+        });
+      },
+      "支付提示",
+      function() {
+        app.alert("拉基吧倒")
+      }
+    )
+
+  }
+
+}
